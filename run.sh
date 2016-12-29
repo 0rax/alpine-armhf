3898cc5d7e0784eaf9bc22e804ef71b5ff4d7cd3
Jean-Philippe Roemer <jp@roemer.im>
2016-12-29 01:36:20 +0100

    Add alpine-armhf:3.5 images [latest stable]

--- run.sh
+++ run.sh
@@ -70,11 +70,20 @@ fail () {
     cc_failure
 }
 
+# AlpineLinux v3.5
+(
+    export LOGDIR
+    export ALPINE_RELEASE=v3.5
+    export IMGTAG="latest ${ALPINE_RELEASE/v/}"
+    export BUILD_DATE=$(date "+%FT%T%z")
+    building
+    ${BUILDPATH}/build.sh && pass || fail
+)
 # AlpineLinux v3.4
 (
     export LOGDIR
     export ALPINE_RELEASE=v3.4
-    export IMGTAG="latest ${ALPINE_RELEASE/v/}"
+    export IMGTAG="${ALPINE_RELEASE/v/}"
     export BUILD_DATE=$(date "+%FT%T%z")
     building
     ${BUILDPATH}/build.sh && pass || fail
